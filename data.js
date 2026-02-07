c4b6ba805fcda044474de08c82515ea0338c49f1
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-02-06 22:53:57 -0500

    upload

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-thin-thn-truyn-tin-cho-c-b-chu-thai",
+      "name": "1. Thiên Thần Truyền Tin Cho Đức Bà Chịu Thai",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1948400729514894,
+          "pitch": 0.05525989617790472,
+          "rotation": 0,
+          "target": "1-2-c-b-i-ving-b-thnh-isave"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.160054435162591,
+          "pitch": -0.28169234161257073,
+          "title": "Truyền tin cho Đức Ma-ri-a",
+          "text": "Bà Ê-li-sa-bét có thai được sáu tháng, thì Thiên Chúa sai sứ thần Gáp-ri-en đến một thành miền Ga-li-lê, gọi là Na-da-rét, gặp một trinh nữ đã thành hôn với một người tên là Giu-se, thuộc dòng dõi vua Đa-vít. Trinh nữ ấy tên là Ma-ri-a.<div><br></div><div>Sứ thần vào nhà trinh nữ và nói: “Mừng vui lên, hỡi Đấng đầy ân sủng, Đức Chúa ở cùng bà.” Nghe lời ấy, bà rất bối rối, và tự hỏi lời chào như vậy có nghĩa gì.</div><div><br></div><div>Sứ thần liền nói: “Thưa bà Ma-ri-a, xin đừng sợ, vì bà đẹp lòng Thiên Chúa. Và này đây bà sẽ thụ thai, sinh hạ một con trai, và đặt tên là Giê-su. Người sẽ nên cao cả, và sẽ được gọi là Con Đấng Tối Cao. Đức Chúa là Thiên Chúa sẽ ban cho Người ngai vàng vua Đa-vít, tổ tiên Người. Người sẽ trị vì nhà Gia-cóp đến muôn đời, và triều đại của Người sẽ vô cùng vô tận.”</div><div><br></div><div>Bà Ma-ri-a thưa với sứ thần: “Việc ấy sẽ xảy ra cách nào, vì tôi không biết đến việc vợ chồng!”</div><div><br></div><div>Sứ thần đáp: “Thánh Thần sẽ ngự xuống trên bà, và quyền năng Đấng Tối Cao sẽ rợp bóng trên bà; vì thế, Đấng Thánh sắp sinh ra sẽ được gọi là Con Thiên Chúa. Kìa bà Ê-li-sa-bét, người họ hàng với bà, tuy già rồi, mà cũng đang cưu mang một người con trai: bà ấy vẫn bị mang tiếng là hiếm hoi, mà nay đã có thai được sáu tháng, vì đối với Thiên Chúa, không có gì là không thể làm được.”</div><div><br></div><div>Bấy giờ bà Ma-ri-a nói: “Vâng, tôi đây là nữ tỳ của Chúa, xin Chúa cứ làm cho tôi như lời sứ thần nói.” Rồi sứ thần từ biệt ra đi.</div><div>(Luca 1:26-38)</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-b-i-ving-b-thnh-isave",
+      "name": "2. Đức Bà Đi Viếng Bà Thánh Isave",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08240088306867044,
+          "pitch": -0.45941287007660847,
+          "rotation": 0,
+          "target": "2-3-c-b-sinh-c-cha-gisu-ni-hang-"
+        },
+        {
+          "yaw": -2.027630697812615,
+          "pitch": -0.37836001166965794,
+          "rotation": 0,
+          "target": "0-1-thin-thn-truyn-tin-cho-c-b-chu-thai"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.12524373470416705,
+          "pitch": -0.6640726414363716,
+          "title": "Đức Ma-ri-a viếng thăm bà Ê-li-sa-bét",
+          "text": "Hồi ấy, bà Ma-ri-a vội vã lên đường, đến miền núi, vào một thành thuộc chi tộc Giu-đa. Bà vào nhà ông Da-ca-ri-a và chào hỏi bà Ê-li-sa-bét. Bà Ê-li-sa-bét vừa nghe tiếng bà Ma-ri-a chào, thì đứa con trong bụng nhảy lên, và bà được đầy tràn Thánh Thần, liền kêu lớn tiếng và nói rằng: “Em được chúc phúc hơn mọi người phụ nữ, và người con em đang cưu mang cũng được chúc phúc. Bởi đâu tôi được Thân Mẫu Chúa tôi đến với tôi thế này? Vì này đây, tai tôi vừa nghe tiếng em chào, thì đứa con trong bụng đã nhảy lên vui sướng. Em thật có phúc, vì đã tin rằng Chúa sẽ thực hiện những gì Người đã nói với em.”<div>(Luca 1:39-45)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-b-sinh-c-cha-gisu-ni-hang-",
+      "name": "3. Đức Bà Sinh Đức Chúa Giêsu Nơi Hang Đá",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11714081544133315,
+          "pitch": -0.15866660765025742,
+          "rotation": 0,
+          "target": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh"
+        },
+        {
+          "yaw": 2.87401091767512,
+          "pitch": -0.2509765799379853,
+          "rotation": 0,
+          "target": "1-2-c-b-i-ving-b-thnh-isave"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16032498761056857,
+          "pitch": -0.4172683295906481,
+          "title": "Đức Giê-su ra đời. Những người chăn chiên đến viếng thăm.",
+          "text": "thực hiện thời ông Qui-ri-ni-ô làm tổng trấn xứ Xy-ri. Ai nấy đều phải về nguyên quán mà khai tên tuổi. Bởi thế, ông Giu-se từ thành Na-da-rét, miền Ga-li-lê lên thành vua Đa-vít tức là Bê-lem, miền Giu-đê, vì ông thuộc gia đình dòng tộc vua Đa-vít. Ông lên đó khai tên cùng với người đã thành hôn với ông là bà Ma-ri-a, lúc ấy đang có thai. Khi hai người đang ở đó, thì bà Ma-ri-a đã tới ngày mãn nguyệt khai hoa. Bà sinh con trai đầu lòng, lấy tã bọc con, rồi đặt nằm trong máng cỏ, vì hai ông bà không tìm được chỗ trong nhà trọ.<div><br></div><div>Trong vùng ấy, có những người chăn chiên sống ngoài đồng và thức đêm canh giữ đàn vật. Bỗng sứ thần Chúa đứng bên họ, và vinh quang của Chúa chiếu tỏa chung quanh, khiến họ kinh khiếp hãi hùng. Nhưng sứ thần bảo họ: “Anh em đừng sợ. Này tôi báo cho anh em một tin mừng trọng đại, cũng là tin mừng cho toàn dân: Hôm nay, một Đấng Cứu Độ đã sinh ra cho anh em trong thành vua Đa-vít, Người là Đấng Ki-tô Đức Chúa. Anh em cứ dấu này mà nhận ra Người: anh em sẽ gặp thấy một trẻ sơ sinh bọc tã, nằm trong máng cỏ.” Bỗng có muôn vàn thiên binh hợp với sứ thần cất tiếng ngợi khen Thiên Chúa rằng:</div><div><br></div><div>“Vinh danh Thiên Chúa trên trời, bình an dưới thế cho loài người Chúa thương.”</div><div><br></div><div>Khi các thiên sứ từ biệt mấy người chăn chiên để về trời, những người này bảo nhau: “Nào chúng ta sang Bê-lem, để xem sự việc đã xảy ra, như Chúa đã tỏ cho ta biết.” Họ liền hối hả ra đi. Đến nơi, họ gặp bà Ma-ri-a, ông Giu-se, cùng với Hài Nhi đặt nằm trong máng cỏ. Thấy thế, họ liền kể lại điều đã được nói với họ về Hài Nhi này. Nghe các người chăn chiên thuật chuyện, ai cũng ngạc nhiên. Còn bà Ma-ri-a thì hằng ghi nhớ mọi kỷ niệm ấy, và suy đi nghĩ lại trong lòng. Rồi các người chăn chiên ra về, vừa đi vừa tôn vinh ca tụng Thiên Chúa, vì mọi điều họ đã được mắt thấy tai nghe, đúng như đã được nói với họ.</div><div><p class=\"MsoNormal\"><span lang=\"VI\">(Lu</span>ca<span lang=\"VI\"> 2:1-20)</span><o:p></o:p></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh",
+      "name": "4. Đức Bà Dâng Đức Chúa Giêsu Trong Đền Thánh",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04448042282405673,
+          "pitch": -0.3609151715076102,
+          "rotation": 0,
+          "target": "4-5-c-b-tm-c-c-cha-gisu-trong-n-thnh"
+        },
+        {
+          "yaw": -2.998004314665394,
+          "pitch": -0.1812815647630721,
+          "rotation": 0,
+          "target": "2-3-c-b-sinh-c-cha-gisu-ni-hang-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004950021509621649,
+          "pitch": -0.5050909207814644,
+          "title": "<p class=\"MsoNormal\">Tiến dâng Đức Giê-su cho\nThiên Chúa<o:p></o:p></p>",
+          "text": "Khi đã đến ngày lễ thanh tẩy của các ngài theo luật Mô-sê, bà Ma-ri-a và ông Giu-se đem con lên Giê-ru-sa-lem, để tiến dâng cho Chúa, như đã chép trong Luật Chúa rằng: “Mọi con trai đầu lòng phải được gọi là của thánh, dành cho Chúa”, và cũng để dâng của lễ theo Luật Chúa truyền, là một đôi chim gáy hay một cặp bồ câu non. Hồi ấy ở Giê-ru-sa-lem, có một người tên là Si-mê-ôn. Ông là người công chính và sùng đạo, ông những mong chờ niềm an ủi của Ít-ra-en, và Thánh Thần hằng ngự trên ông. Ông đã được Thánh Thần linh báo cho biết là ông sẽ không thấy cái chết trước khi được thấy Đấng Ki-tô của Đức Chúa. Được Thần Khí thúc đẩy, ông lên Đền Thờ. Vào lúc cha mẹ Hài Nhi Giê-su đem con tới để chu toàn tập tục Luật đã truyền liên quan đến Người, thì ông ẵm lấy Hài Nhi trên tay, và chúc tụng Thiên Chúa rằng:<div><br></div><div>Bài ca “An Bình Ra Đi” (Nunc dimittis)</div><div><br></div><div>“Muôn lạy Chúa, giờ đây theo lời Ngài đã hứa, xin để tôi tớ này được an bình ra đi. Vì chính mắt con được thấy ơn cứu độ Chúa đã dành sẵn cho muôn dân: Đó là ánh sáng soi đường cho dân ngoại, là vinh quang của Ít-ra-en Dân Ngài.”</div><div><br></div><div>Ông Si-mê-ôn nói tiên tri</div><div><br></div><div>Cha và mẹ Hài Nhi ngạc nhiên vì những lời ông Si-mê-ôn vừa nói về Người. Ông Si-mê-ôn chúc phúc cho hai ông bà, và nói với bà Ma-ri-a, mẹ của Hài Nhi: “Thiên Chúa đã đặt cháu bé này làm duyên cớ cho nhiều người Ít-ra-en ngã xuống hay đứng lên. Cháu còn là dấu hiệu cho người đời chống báng; và như vậy, những ý nghĩ từ thâm tâm nhiều người sẽ lộ ra. Còn chính bà, một lưỡi gươm sẽ đâm thâu tâm hồn bà.”</div><div><br></div><div>Bà An-na nói tiên tri</div><div><br></div><div>Lại cũng có một nữ ngôn sứ tên là An-na, con ông Pơ-nu-ên, thuộc chi tộc A-se. Bà đã nhiều tuổi lắm. Từ khi xuất giá, bà đã sống với chồng được bảy năm, rồi ở góa, đến nay đã tám mươi tư tuổi. Bà không rời bỏ Đền Thờ, những ăn chay cầu nguyện, sớm hôm thờ phượng Thiên Chúa. Cũng vào lúc ấy, bà tiến lại gần bên, cảm tạ Thiên Chúa, và nói về Hài Nhi cho hết những ai đang mong chờ ngày Thiên Chúa cứu chuộc Giê-ru-sa-lem.</div><div><p class=\"MsoNormal\">(Luca 2:22-38)<o:p></o:p></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-b-tm-c-c-cha-gisu-trong-n-thnh",
+      "name": "5. Đức Bà Tìm Được Đức Chúa Giêsu Trong Đền Thánh",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.224982886550988,
+          "pitch": 0.01823191313473771,
+          "rotation": 0,
+          "target": "3-4-c-b-dng-c-cha-gisu-trong-n-thnh"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.15803943551415855,
+          "pitch": -0.41004943600042054,
+          "title": "Đức Giê-su ngồi giữa các bậc thầy Do-thái",
+          "text": "Hằng năm, cha mẹ Đức Giê-su trẩy hội đền Giê-ru-sa-lem mừng lễ Vượt Qua. Khi Người được mười hai tuổi, cả gia đình cùng lên đền, như người ta thường làm trong ngày lễ. Xong kỳ lễ, hai ông bà trở về, còn cậu bé Giê-su thì ở lại Giê-ru-sa-lem, mà cha mẹ chẳng hay biết. Ông bà cứ tưởng là cậu về chung với đoàn lữ hành, nên sau một ngày đường, mới đi tìm kiếm giữa đám bà con và người quen thuộc. Không thấy con đâu, hai ông bà trở lại Giê-ru-sa-lem mà tìm. Sau ba ngày, hai ông bà mới tìm thấy con trong Đền Thờ, đang ngồi giữa các thầy dạy, vừa nghe họ, vừa đặt câu hỏi. Ai nghe cậu nói cũng ngạc nhiên về trí thông minh và những lời đối đáp của cậu. Khi thấy con, hai ông bà sửng sốt, và mẹ Người nói với Người: “Con ơi, sao con lại xử với cha mẹ như vậy? Con thấy không, cha con và mẹ đây đã phải cực lòng tìm con!” Người đáp: “Sao cha mẹ lại tìm con? Cha mẹ không biết là con có bổn phận ở nhà của Cha con sao?” Nhưng ông bà không hiểu lời Người vừa nói.<div>(Luca 2:41-50)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
